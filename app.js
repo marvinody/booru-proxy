0f64feb301221780d92c68b45e7755615097c04f
Manuel Bugallo <marvinody@gmail.com>
2019-10-24 11:52:30 -0400

    feat: inertial kermit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+
+app.use(require('morgan')('dev'))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.get('/:url', (req, res, next) => {
+  axios({
+    method: 'get',
+    url: `https://danbooru.donmai.us/data/${req.params.url}`,
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      response.data.pipe(res)
+    }).catch(err => {
+      res.send(err)
+    })
+})
+
+const port = process.env.PORT || 7000
+app.listen(port, () => {
+  console.log('listening on ' + port)
+})
